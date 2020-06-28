4a98d22658192daf5dd3050bf1d0d2b4bef88d09
ya.besschetnov <67239252+besschetnov@users.noreply.github.com>
2020-06-28 09:17:26 +0300

    Update FindQueryByFragment.sh

--- ТЖ/Скрипты анализа ТЖ/FindQueryByFragment.sh
+++ ТЖ/Скрипты анализа ТЖ/FindQueryByFragment.sh
@@ -1,8 +1,8 @@
-cat *.log | 
-sed -r '/^p_[0-9]+: /d' | 
-sed -r 's/#tt[0-9]+/#tt/g' |
-sed -r 's/\xef\xbb\xbf//g' |
-sed -r 's/^[0-9]+:[0-9]+\.[0-9]+-[0-9]+,Context,.,Context=/Context=/' |
+cat rphost*/*.log | 
+perl -pe '/^p_[0-9]+: /d' | 
+perl -pe 's/#tt[0-9]+/#tt/g' |
+perl -pe 's/\xef\xbb\xbf//g' |
+perl -pe 's/^[0-9]+:[0-9]+\.[0-9]+-[0-9]+,Context,.,Context=/Context=/' |
 perl -pe 's/\n/@/g; s/\s/ /; s/^[0-9]+:[0-9]+\.[0-9]+-[0-9]+/\n/' | 
 grep -P 'T1._IDRRef' |
 uniq -c |
